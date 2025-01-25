d277181093adbfdcb5b1d2ba82271849bd82975b
Filip Ryan <146904648+raincuhh@users.noreply.github.com>
2025-01-25 10:05:50 -0800

    new changes

--- src/shared/components/layouts/appLayout.tsx
+++ src/shared/components/layouts/appLayout.tsx
@@ -4,7 +4,7 @@ type AppLayoutProps = PropsWithChildren;
 
 const AppLayout = ({ children }: AppLayoutProps): React.JSX.Element => {
 	return (
-		<div className="min-h-dvh h-dvh basis-0 flex-1 leading-relaxed">
+		<div className="min-h-dvh h-dvh basis-0 flex-1 leading-relaxed bg-primary">
 			<div className="flex h-full">{children}</div>
 		</div>
 	);
