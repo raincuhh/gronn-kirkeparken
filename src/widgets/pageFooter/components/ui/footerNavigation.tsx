951de0c6f3fca43b50b9f7e989603d551869d8ab
Filip Ryan <146904648+raincuhh@users.noreply.github.com>
2025-02-07 06:01:22 -0800

    sigma navigation

---
+++ src/widgets/pageFooter/components/ui/footerNavigation.tsx
@@ -0,0 +1,67 @@
+import RenderList from "@/shared/components/utils/renderList";
+import React from "react";
+import { Link } from "react-router-dom";
+
+type FooterSection = {
+	title: string;
+	links: { title: string; href: string }[];
+};
+
+type FooterNavigationProps = {
+	sections: FooterSection[];
+	isAdmin: boolean;
+	adminLinks: FooterSection;
+};
+
+const FooterNavigation = ({ sections, isAdmin, adminLinks }: FooterNavigationProps): React.JSX.Element => {
+	return (
+		<>
+			<RenderList
+				data={sections}
+				render={(item: FooterSection, i: number) => (
+					<div key={i}>
+						<h2 className="text-md text-text-normal !mt-4 !mb-2 font-xl">{item.title}</h2>
+						<ul>
+							<RenderList
+								data={item.links}
+								render={(row: { title: string; href: string }, i) => (
+									<li
+										key={i}
+										className="text-text-muted !py-1 hover:text-text-normal font-lg transition-colors duration-100 ease-in-out"
+									>
+										<Link to={row.href}>{row.title}</Link>
+									</li>
+								)}
+							/>
+						</ul>
+					</div>
+				)}
+			/>
+			{isAdmin && (
+				<RenderList
+					data={[adminLinks]}
+					render={(item: FooterSection, i: number) => (
+						<div key={i}>
+							<h2 className="text-md text-text-normal !mt-4 !mb-2 font-xl">{item.title}</h2>
+							<ul>
+								<RenderList
+									data={item.links}
+									render={(row: { title: string; href: string }, i) => (
+										<li
+											key={i}
+											className="text-text-muted !py-1 hover:text-text-normal font-lg transition-colors duration-100 ease-in-out"
+										>
+											<Link to={row.href}>{row.title}</Link>
+										</li>
+									)}
+								/>
+							</ul>
+						</div>
+					)}
+				/>
+			)}
+		</>
+	);
+};
+
+export default FooterNavigation;
--- src/widgets/pageFooter/components/ui/pageFooter.tsx
+++ src/widgets/pageFooter/components/ui/pageFooter.tsx
@@ -1,17 +1,66 @@
 import React from "react";
 import clsx from "clsx";
+import { Link } from "react-router-dom";
+import FooterNavigation from "./footerNavigation";
 
 type PageFooterProps = { isPrivateRoute: boolean };
+
+const footerLinks = [
+	{
+		title: "Navigasjon",
+		links: [
+			{ title: "Hjem", href: "/home" },
+			{ title: "Kunngjøringer", href: "/announcements" },
+			{ title: "Fotoalbum", href: "/photo-album" },
+		],
+	},
+	{
+		title: "Konto",
+		links: [
+			{ title: "Logg inn", href: "/login" },
+			{ title: "Registrer", href: "/register" },
+			{ title: "Dashboard", href: "/dashboard" },
+		],
+	},
+	{
+		title: "Sosiale Medier",
+		links: [
+			{ title: "Facebook", href: "https://facebook.com" },
+			{ title: "Instagram", href: "https://instagram.com" },
+			{ title: "Twitter", href: "https://twitter.com" },
+		],
+	},
+];
+
+const adminLinks = {
+	title: "Admin",
+	links: [
+		{ title: "Godkjenn bilder", href: "/dashboard/photo-approvals" },
+		{ title: "Administrer kunngjøringer", href: "/dashboard/announcements" },
+	],
+};
 
 const PageFooter = ({ isPrivateRoute }: PageFooterProps): React.JSX.Element => {
 	return (
 		<footer
 			className={clsx(
-				"md:px-16 px-4 flex flex-row justify-between w-full max-w-[1020px] mx-auto items-center",
+				"md:px-16 px-4 my-16 flex flex-row justify-between w-full max-w-[1020px] mx-auto items-center",
 				isPrivateRoute ? "" : ""
 			)}
 		>
-			<div>foooter</div>
+			<nav className="flex flex-col w-full gap-16">
+				<div className="grid-cols-2 md:grid-cols-4 grid gap-12 w-full">
+					<FooterNavigation sections={footerLinks} isAdmin={true} adminLinks={adminLinks} />
+					<Link to={"/home"}>
+						<img className=" w-42 md:w-48 h-auto" src="/assets/images/kirkeparkenLogo.svg" alt="logo" />
+					</Link>
+				</div>
+				<div className="flex justify-between">
+					<p className="text-sm font-md text-text-muted">
+						&copy; {new Date().getFullYear()} Kirkeparken VGS
+					</p>
+				</div>
+			</nav>
 		</footer>
 	);
 };
